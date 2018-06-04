c92838cfb4ddc53660abfc624ef64c8c0ab4d0c5
mikevhe18 <viriyananda.michael@gmail.com>
2018-06-04 16:37:36 +0700

    [8.0.1.0.0][proxy_backend_aeroo]

---
+++ proxy_backend_aeroo/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Hardware Proxy for Backend Aeroo",
+    "version": "8.0.1.0.0",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "proxy_backend",
+        "report_aeroo",
+        "base_other_report_engines"
+    ],
+    "data": [
+    ],
+}
