75823110b3cef4fea385816f704e227772027a64
mikevhe18 <viriyananda.michael@gmail.com>
2018-06-04 16:38:05 +0700

    [8.0.1.0.0][proxy_backend_cups_aeroo]

---
+++ proxy_backend_cups_aeroo/static/src/js/main.js
@@ -0,0 +1,7 @@
+openerp.proxy_backend_cups_aeroo = function(instance) {
+
+    var module = instance.proxy_backend;
+
+	proxy_backend_cups_aeroo_device(instance,module);
+    proxy_backend_cups_aeroo_print(instance,module);
+};
