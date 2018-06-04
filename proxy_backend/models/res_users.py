fbe533cef4ccf0ca7718e8075b7edbcf02ca039b
mikevhe18 <viriyananda.michael@gmail.com>
2018-06-04 16:37:12 +0700

    [8.0.1.1.1][proxy_backend]

--- proxy_backend/models/res_users.py
+++ proxy_backend/models/res_users.py
@@ -6,7 +6,14 @@ from openerp import models, fields
 
 
 class ResUsers(models.Model):
-    _inherit = 'res.users'
+    _inherit = "res.users"
+
+    @classmethod
+    def _build_model(cls, pool, cr):
+        model = super(ResUsers, cls)._build_model(pool, cr)
+        ModelCls = type(model)
+        ModelCls.SELF_WRITEABLE_FIELDS += ["proxy_backend_id"]
+        return model
 
     proxy_backend_id = fields.Many2one(
         comodel_name="proxy.backend",
