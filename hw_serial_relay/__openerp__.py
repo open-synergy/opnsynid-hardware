8b90045b701543bdea759e113914c37c62505643
Andhitia Rama <andhitia.r@gmail.com>
2018-06-22 16:52:32 +0700

    [8.0.1.0.0] hw_serial_relay

---
+++ hw_serial_relay/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "HW Proxy - Serial Relay",
+    "summary": "HW Proxy - Serial Relay",
+    "version": "8.0.1.0.1",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "depends": [
+        "hw_proxy"
+    ],
+    "external_dependencies": {
+        "python": [
+            "serial",
+        ],
+    },
+    "installable": True,
+}
