2eae9dc9673ef7ec61e330d16898f49df5d1ebb7
Andhitia Rama <andhitia.r@gmail.com>
2018-06-29 22:08:44 +0700

    [8.0.1.4.0] proxy_backend
    * Arrange device menu into sub-folder to accomodate different type of device
    * Default method for type_id field
    * Code refactoring

--- proxy_backend/models/proxy_backend_device.py
+++ proxy_backend/models/proxy_backend_device.py
@@ -2,21 +2,26 @@
 # Copyright 2018 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class ProxyBackendDevice(models.Model):
     _name = "proxy.backend_device"
     _description = "Proxy Backend Device"
 
+    @api.model
+    def _default_device_type(self):
+        return False
+
     name = fields.Char(
-        string="Serial Number",
+        string="Device Name",
         required=True,
     )
     type_id = fields.Many2one(
         string="Device Type",
         comodel_name="proxy.backend_device_type",
         required=True,
+        default=lambda self: self._default_device_type(),
     )
     proxy_backend_id = fields.Many2one(
         string="Proxy Backend",
