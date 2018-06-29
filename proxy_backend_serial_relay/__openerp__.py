cc635dfb37135a59d5dcf970878b090389adb96f
Andhitia Rama <andhitia.r@gmail.com>
2018-06-29 23:25:24 +0700

    [8.0.1.0.0] proxy_backend_serial_relay

---
+++ proxy_backend_serial_relay/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Use Serial Relay From Backend View",
+    "version": "8.0.1.0.0",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "proxy_backend"
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/proxy_backend_device_type_data.xml",
+        "views/proxy_backend_serial_relay.xml",
+        "views/proxy_backend_serial_relay_views.xml",
+    ],
+}
