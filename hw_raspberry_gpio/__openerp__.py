cc8513d12141c1ec14beaf36cebef3ba668f54fe
Andhitia Rama <andhitia.r@gmail.com>
2018-06-29 11:18:41 +0700

    [hw_raspberry_gpio] Change module name

--- hw_raspberry_gpio/__openerp__.py
+++ hw_raspberry_gpio/__openerp__.py
@@ -2,7 +2,7 @@
 # Copyright 2018 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Raspberry Pi GPIO Hardware Proxy",
+    "name": "HW Proxy - Raspberry Pi GPIO",
     "version": "8.0.1.0.1",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
