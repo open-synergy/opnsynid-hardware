101d9fbac795010c209968d9ed452b52dd57b969
Andhitia Rama <andhitia.r@gmail.com>
2018-06-29 11:17:04 +0700

    [hw_proxy_cups] Change module name

--- hw_proxy_cups/__openerp__.py
+++ hw_proxy_cups/__openerp__.py
@@ -2,7 +2,7 @@
 # Copyright 2018 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Cups Hardware Driver",
+    "name": "HW Proxy - CUPS",
     "version": "8.0.1.0.0",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
