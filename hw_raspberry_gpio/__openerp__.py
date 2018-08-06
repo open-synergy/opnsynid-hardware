9f183a43e9bb81e8e99d17b5f910ccc1f8299f5e
mikevhe18 <viriyananda.michael@gmail.com>
2018-08-06 11:20:50 +0700

    [8.0.1.1.0][hw_raspberry_gpio]

--- hw_raspberry_gpio/__openerp__.py
+++ hw_raspberry_gpio/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "HW Proxy - Raspberry Pi GPIO",
-    "version": "8.0.1.0.1",
+    "version": "8.0.1.1.0",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
     "license": "AGPL-3",
