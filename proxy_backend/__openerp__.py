acd9117cfe47cbc15ae3fb5a099db4475f65144c
mikevhe18 <viriyananda.michael@gmail.com>
2018-08-06 09:10:26 +0700

    [8.0.1.5.0][proxy_backend]

--- proxy_backend/__openerp__.py
+++ proxy_backend/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Hardware Proxy for Backend",
-    "version": "8.0.1.4.1",
+    "version": "8.0.1.5.0",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
     "license": "AGPL-3",
