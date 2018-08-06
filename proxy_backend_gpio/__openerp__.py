c3ce39be4f4f78bc30a80336dc2942b7ac3a4079
mikevhe18 <viriyananda.michael@gmail.com>
2018-08-06 11:21:42 +0700

    [8.0.1.2.0][proxy_backend_gpio]

--- proxy_backend_gpio/__openerp__.py
+++ proxy_backend_gpio/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Backend Using Hardware Proxy + GPIO",
-    "version": "8.0.1.1.0",
+    "version": "8.0.1.2.0",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
     "license": "AGPL-3",
@@ -14,7 +14,7 @@
     "data": [
         "security/ir.model.access.csv",
         "data/proxy_backend_device_type_data.xml",
-        "views/proxy_backend_raspberry_relay_views.xml",
-        "views/proxy_backend_gpio.xml"
+        "views/proxy_backend_gpio.xml",
+        "views/proxy_backend_raspberry_relay_views.xml"
     ],
 }
