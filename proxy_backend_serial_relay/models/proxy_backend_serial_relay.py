1df1b07099e8289748f69dd1937fd779edbc1844
mikevhe18 <viriyananda.michael@gmail.com>
2018-08-06 09:10:46 +0700

    [8.0.1.3.0][proxy_backend_serial_relay]

--- proxy_backend_serial_relay/models/proxy_backend_serial_relay.py
+++ proxy_backend_serial_relay/models/proxy_backend_serial_relay.py
@@ -10,23 +10,13 @@ class ProxyBackendSerialRelay(models.Model):
     _inherits = {"proxy.backend_device": "device_id"}
     _description = "Proxy Backend Device - Serial Relay"
 
-    @api.model
-    def _default_device_type(self):
-        return self.env.ref("proxy_backend_serial_relay."
-                            "proxy_backend_device_type_"
-                            "arduino_serial_relay").id
-
     device_id = fields.Many2one(
         string="Device",
         comodel_name="proxy.backend_device",
         required=True,
         ondelete="cascade",
+        auto_join=True
     )
-    dummy_device_id = fields.Many2one(
-        string="ID Device",
-        comodel_name="proxy.backend_device",
-        related="device_id",
-        )
     device_path = fields.Char(
         string="Device Path",
         required=True,
