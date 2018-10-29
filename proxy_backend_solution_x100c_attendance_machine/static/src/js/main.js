adf8252ff9dbdf3d7e8704529a6d5fafc2a881f3
Andhitia Rama <andhitia.r@gmail.com>
2018-10-29 20:59:08 +0700

    [8.0.1.0.0] proxy_backend_solution_x100c_attendance_machine

---
+++ proxy_backend_solution_x100c_attendance_machine/static/src/js/main.js
@@ -0,0 +1,6 @@
+openerp.proxy_backend_solution_x100c_attendance_machine = function(instance) {
+
+    var module = instance.proxy_backend;
+    proxy_backend_solution_x100c_attendance_machine_action(instance, module);
+
+};
