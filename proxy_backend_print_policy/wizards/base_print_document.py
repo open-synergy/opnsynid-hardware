b12c9f2c50339803eb53992039d28d41baab4780
mikevhe18 <viriyananda.michael@gmail.com>
2019-12-23 11:20:53 +0700

    Revision

--- proxy_backend_print_policy/wizards/base_print_document.py
+++ proxy_backend_print_policy/wizards/base_print_document.py
@@ -2,7 +2,7 @@
 # Copyright 2019 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api
+from openerp import fields, models, api
 
 
 class BasePrintDocument(models.TransientModel):
